c374b579b15cefbece724ebea472a569a654b162
Erik Öjebo <erik.ojebo@gmail.com>
2012-10-23 22:05:27 +0200

    added working sample which uses templatefile task to set configuration settings depending on the supplied environment. The script is integrated into the build of the project.

---
+++ src/MsbuildExample/MsbuildExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace MsbuildExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Connection string: " + ConfigurationManager.ConnectionStrings["DefaultConnectinoString"]);
+            Console.WriteLine("SomeSetting: " + ConfigurationManager.AppSettings["SomeSetting"]);
+        }
+    }
+}
