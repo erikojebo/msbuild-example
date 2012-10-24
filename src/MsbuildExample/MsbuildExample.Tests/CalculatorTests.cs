6ea6e99aa697967bd3f362551593246b5854f9d6
Erik Öjebo <erik.ojebo@gmail.com>
2012-10-24 09:31:24 +0200

    Added test project with single simple test

---
+++ src/MsbuildExample/MsbuildExample.Tests/CalculatorTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace MsbuildExample.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Add_returns_the_sum_of_the_parameters()
+        {
+            Assert.AreEqual(3, Calculator.Add(1,2));
+        }
+    }
+}
